510ee551f36be56090867f6e119659ae092d0ff4
Anmol Verma <anmolverma102002@gmail.com>
2023-01-28 19:26:03 +0530

    feat: :camera: added image generation feature

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
-const PORT = 3001;
+const PORT = 8080;
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post',postRoutes);
+app.use('/api/v1/dalle',dalleRoutes);
 
 app.get('/', (req, res) => {
 	res.send('Hello from AI-Art');
